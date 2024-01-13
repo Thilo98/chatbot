0594f54cb713707b5a1b87d8acce62f044f23b58
Thilo98 <136798599+Thilo98@users.noreply.github.com>
2024-01-13 09:51:01 +0530

    main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ st.set_page_config(
 
 st.secrets["api_key"]
 
-api_key = "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"
+
 # since we doont to keep runing frontend code while reciving from openai without intrepuuting while waiting
 # to response to come we us async function.
 async def main():
